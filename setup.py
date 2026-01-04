85a8519bac361f6dd52b8b2e2af194c42923499a
vibhas garg <vibhasgarg70@gmail.com>
2026-01-04 11:29:57 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+
+
+setup(
+name= 'mlproject',
+version='0.0.1',
+author='Vibhas',
+auther_email='vibhasgarg70@gmail',
+packages=find_packages(),
+install_requires=['pandas','numpy','seaborn']
+
+
+
+)
